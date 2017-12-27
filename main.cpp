94a5fe59cb03e5d767b67354a70742574346d8a3
antonMeln <32076906+antonMeln@users.noreply.github.com>
2017-12-27 18:46:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,49 +7,52 @@ using namespace std;
 bool read (int & n, int * mas)
 {
   string str;
-   getline(cin,str);
-    istringstream stream(str);
-     bool F = true;
+  getline(cin,str);
+  istringstream stream(str);
+  bool F = true;
     
-       for (int i=0;i<n;i++)
-        {
-          if (!(stream >> mas[i])) { F = false; break;}
-        }
+      for (int i=0;i<n;i++){
+       
+       if (!(stream >> mas[i])) {
+         F = false; 
+         break;
+         
+       }
+      }
    return F;
 }
 
 int main ()
  { 
-   int * mas = new int; 
    int i,j,n;
+   int * mas = new int[n]; 
    int min;
    string str;
    getline(cin,str);
    istringstream  stream (str);
     
        if (stream >> n){
-         if (read(n,mas)){
-          int min_i;
-          for (int i = 0;i <n;i++){
-            min = mas[i];
-            min_i = i;
-             for (int j = i; j < n; j++){
-               if (mas[j]< min) {
-                 min = mas[j];
-                 min_i = j;
-               }
+          if (read(n,mas)){
+            int min_i;
+            for (int i = 0;i <n;i++){
+              min = mas[i];
+              min_i = i;
+              for (int j = i; j < n; j++){
+                if (mas[j]< min) {
+                  min = mas[j];
+                  min_i = j;
+                }
                
-             }
-             swap(mas[i],mas[min_i]);
-          }
-              for (int i = 0;i <n;i++) 
-                 cout << mas[i] << ' ';
+              }
+              swap(mas[i],mas[min_i]);
+            }
+            for (int i = 0;i <n;i++) 
+              cout << mas[i] << ' ';
                 
+          }
+          else cout << "An error has occured while reading input data.";
            
-         }
-         else cout << "An error has occured while reading input data.";
-           
-}
-        
- return 0;
+  }
+  delete[] mas;      
+  return 0;
  }  
